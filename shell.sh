7c6a4dd6487bca6f1244e59e2131d7db3d3af665
sireeshasiri3008 <tallapureddysireesha@gmail.com>
2025-10-25 22:31:32 +0530

    conflicts resolved

--- shell.sh
+++ shell.sh
@@ -1,4 +1,21 @@
 echo "Hello-world!1"
 echo "first project"
+<<<<<<< HEAD
+echo "first project develop branch"
+echo "first project develop branch 2nd update"
+
+
+gitlab-
+Azuredevops-repos,pipelines
+
+souce
+pipelines
+build artifacts
+play Store
+App Store
+Veracode - agent scan, SCA
+saucelabs- 90 days,
+thru
 <<<<<<< TAIL test actual is HEAD
 echo "first project develop branch 2nd update"
+>>>>>>> 131c6a17a0daffff5145c5fa5d27397ff4dec7c0
