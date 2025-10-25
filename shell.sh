6c57e2d344af7d2ce857e861269750e66ca39f01
sireeshasiri3008 <tallapureddysireesha@gmail.com>
2025-10-25 21:40:02 +0530

    SIRI-1213: shell.sh update

--- shell.sh
+++ shell.sh
@@ -2,3 +2,17 @@ echo "Hello-world!1"
 echo "first project"
 echo "first project develop branch"
 echo "first project develop branch 2nd update"
+
+
+gitlab-
+Azuredevops-repos,pipelines
+
+souce
+pipelines
+build artifacts
+play Store
+App Store
+Veracode - agent scan, SCA
+saucelabs- 90 days,
+thru
+
